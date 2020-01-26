7e939771c418051fedf6b3e07e4fb4c8bd58fbe4
PeterGhimself <petergranitski@gmail.com>
2020-01-26 03:57:26 -0500

    Remove unrelated code

--- scrape_schedule.py
+++ scrape_schedule.py
@@ -12,71 +12,21 @@ import re
 # custom modules
 import userconfig as cfg
 
-new_branch_name = 'testy'
-# in case multiple branch names passed at once
-new_branch_names = []
-
-# if passing multiple branch names then as a single string
-# with comma separated values and no spaces
-MULTI_ARG_DELIMETER = ','
-
-# list option variables
-list_branches = False
-LIST_FLAGS = ['--list', '-l']
-## user feedback messages
-UPDATE_MODE_MSG = 'Update mode selected'
-LIST_MODE_MSG = 'List mode selected'
-LIST_FLAG_TYPO_MSG = 'Did you mean --list or -l? '
-EXIT_PROMPT = 'q to quit now '
-USER_INPUT_OPTIONS = '(y/n/q): '
-EXIT_OPTION_MSG = 'Exiting script'
-
-# file meant to keep track of the latest branch values
-# gets overwritten very time branches are updated/listed
-BRANCH_LIST_FILE = 'updated_branch_list'
-
 # HTML id/classes
 NETNAME_FIELD_ID = 'userid'
 PASSWORD_FIELD_ID = 'pwd'
 EMAIL_TEXT_ID = 'DERIVED_SSS_SCL_EMAIL_ADDR'
 IFRAME_ID = 'ptifrmtgtframe'
 SCHEDULE_BOX_ID = 'STDNT_WEEK_SCHD$scroll$0'
+
 # in seconds
-PAGE_LOAD_WAIT_TIME = 5
+PAGE_LOAD_WAIT_TIME = 4
 
 # regexes
 course_code_rgx = re.compile('[A-Z]{4}\s\d{3,4}')
 timeslot_rgx = re.compile('')
 
-#@TODO: add usage string
-
-if len(sys.argv) > 1:
-    new_branch_name = sys.argv[1]
-
-    if new_branch_name in LIST_FLAGS:
-        list_branches = True
-        print(LIST_MODE_MSG)
-    # try to catch typos
-    elif 'list' in new_branch_name:
-        answer = input(LIST_FLAG_TYPO_MSG + '\n' + EXIT_PROMPT + USER_INPUT_OPTIONS)
-        if answer.lower() == 'y':
-            list_branches = True
-            print(LIST_MODE_MSG)
-        elif answer.lower() == 'q':
-            print(EXIT_OPTION_MSG)
-            sys.exit(0)
-        else:
-            print(UPDATE_MODE_MSG)
-    else:
-        # check if multiple branch names
-        if MULTI_ARG_DELIMETER in new_branch_name:
-            print('Multiple branch names detected')
-            new_branch_names = new_branch_name.split(MULTI_ARG_DELIMETER)
-            # remove duplicates
-            new_branch_names = list(dict.fromkeys(new_branch_names))
-
-            for branch in new_branch_names:
-                print("Found: " + branch)
+#@TODO: add usage string ?
 
 current_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
 chromedriver_path = current_dir + 'chromedriver'
@@ -126,7 +76,7 @@ if cfg.user['netname'] and cfg.user['password']:
 
     # wait just a bit, otherwise line below is flaky
     #@TODO: do a real fix for the netname.clear() bug
-    time.sleep(3) # this line does not seem to help as it still flakes on the following line sometimes
+    time.sleep(PAGE_LOAD_WAIT_TIME) # this line does not seem to help as it still flakes on the following line sometimes
     netname.clear()
     netname.send_keys(user_netname)
     password.clear()
@@ -161,7 +111,7 @@ try:
         sys.exit()
 
     print('waiting for page to load for ' + str(PAGE_LOAD_WAIT_TIME) + ' seconds')
-    time.sleep(PAGE_LOAD_WAIT_TIME)    
+    time.sleep(PAGE_LOAD_WAIT_TIME)
 
     # start scraping for stuff on the current page
     print('locating user email address...')
@@ -177,12 +127,10 @@ try:
     user_info['netname'] = user_netname
     user_info['email'] = email
    
-    schedule_query = iframeContext + ".getElementById('" + SCHEDULE_BOX_ID + "').innerText"
-    schedule_query += ".split('\\n').filter((value, index, array) => {return (value != null && !value.trim() == '')});"
+    get_schedule_text = iframeContext + ".getElementById('" + SCHEDULE_BOX_ID + "').innerText"
+    get_schedule_text += ".split('\\n').filter((value, index, array) => {return (value != null && !value.trim() == '')});"
 
-    #print('schedule_query', schedule_query)
-
-    schedule = driver.execute_script(schedule_query)
+    schedule = driver.execute_script(get_schedule_text)
     
     print('schedule', schedule)
 
@@ -239,7 +187,7 @@ try:
     program_link_click = iframeContext + ".getElementById('DERIVED_SSS_SCL_SSS_MORE_ADVISOR$162$').click()"
     driver.execute_script(program_link_click)
 
-    time.sleep(5)
+    time.sleep(PAGE_LOAD_WAIT_TIME)
 
     print('Page title: ', driver.title)
     
@@ -258,7 +206,7 @@ try:
     click_student_center = "document.getElementById('pthnavbccrefanc_HC_SSS_STUDENT_CENTER').click()"
     # go back home so we can retrieve the id
     driver.execute_script(click_student_center)
-    time.sleep(3)
+    time.sleep(PAGE_LOAD_WAIT_TIME)
 
     if driver.title == "Student Center":
         print('Sucessfuly changed to Student Center page')
@@ -268,7 +216,7 @@ try:
 
     click_demographic_data = iframeContext + ".getElementById('DERIVED_SSS_SCL_SS_DEMO_SUM_LINK').click()"
     driver.execute_script(click_demographic_data)
-    time.sleep(3)
+    time.sleep(PAGE_LOAD_WAIT_TIME)
 
     if driver.title == "Demographic Information":
         print('Sucessfuly changed to Demographic Information page')
@@ -292,63 +240,6 @@ try:
 
     #@TODO: refactor all these time.sleeps to using polling and stop when DOM is ready
     #@TODO: end of web scraper! Delete everything following this line after 'finished'
-
-    # so let's try javscripting this instead
-    print('inserting value: ', new_branch_name)
-    #TODO: try to implement it properly as shown in 'get_branch_list.js' vs this hack
-    branches_val = "document.getElementsByName('branches[]')[2].value"
-
-    
-    if list_branches:
-        get_branches = 'return ' + branches_val 
-        branch_list = driver.execute_script(get_branches)
-        print('listing current branches:')
-        print(branch_list)
-
-        # save list to file
-        with open(BRANCH_LIST_FILE, 'w+') as f:
-            f.write(branch_list + "\n")
-
-    else:
-        # for the TODO fix mentioned above
-        """branches_val = ''
-        with open('./get_branch_list.js', 'r') as js:
-            # 3) Read the jquery from a file
-            branches_val = js.read()
-
-        get_branches = "return " + branches_val
-        updated_branch_list = driver.execute_script(get_branches)
-
-        # visual confirmation
-        print('updated branch list: ', updated_branch_list)
-        """
-        set_branches = branches_val + " += '"
-        if len(new_branch_names) > 0:
-            for branch in new_branch_names:
-                set_branches += ', ' + branch
-            set_branches += "'"
-        else:
-            set_branches = branches_val + " += ', " + new_branch_name + "'"
-        print('set_branches', set_branches)
-        get_branches = "return " + branches_val
-        driver.execute_script(set_branches)
-        updated_branch_list = driver.execute_script(get_branches)
-
-        # visual confirmation
-        print('updated branch list: ', updated_branch_list)
-
-        # save list to file
-        with open(BRANCH_LIST_FILE, 'w+') as f:
-            f.write(updated_branch_list + "\n")
-
-        # necessary wait, otherwise save button gets clicked too fast to actually work
-        time.sleep(0.5)
-
-        # only parent is clickable with selenium driver
-        # although in js child is clickable
-        print('locating save button...')
-        save = driver.find_element_by_id('add_integration_parent')
-        save.click()
 
 except Exception as err:
     print('Error encountered!')
